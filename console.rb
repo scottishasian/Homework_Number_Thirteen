a2a150347a30e178eac9935a586dd7d6d29b9172
Kynan <k.song@blueyonder.co.uk>
2017-10-11 19:50:29 +0100

    Added 2 albums to console.rb

--- console.rb
+++ console.rb
@@ -1,9 +1,26 @@
 require ( 'pry' )
 require_relative( 'models/artist.rb' )
+require_relative( 'models/album.rb' )
 
 artist_1 = Artist.new({'name' => 'Alt-J'})
 
 artist_2 = Artist.new({'name' => 'Cream'})
+
+album_1 = Album.new(
+  {
+    'artist_id' => artist_1.id,
+    'album_title' => 'An Awesome Wave',
+    'genre' => 'Art Rock'
+  }
+)
+
+album_2 = Album.new(
+  {
+    'artist_id' => artist_2.id,
+    'album_title' => 'Disraeli Gears',
+    'genre' => 'Rock'
+  }
+)
 
 
 binding.pry
